ce944202117eff7c8fe012c05e17395049f68f22
KyrillWil <kyrill.willems@student.tsaam.be>
2025-09-03 10:17:07 +0200

    commit1

--- Javascript/oefening1.js
+++ Javascript/oefening1.js
@@ -1,1 +1,26 @@
 // elke oefening in een appart bestand
+
+//number
+let getal = 1
+//string
+const naam = "Kyrill"
+//numver
+const komma = 0.1
+//undifined
+const geen = undefined
+//boolean
+const ja = true
+//null
+let leeg = null
+//array
+const namenLijst = {}
+//object
+const winkels = []
+
+//test
+console.log(typeof getal);
+console.log(typeof naam);
+console.log(typeof komma);
+console.log(typeof geen);
+console.log(typeof ja);
+console.log(typeof leeg);
--- Python/oefening1.py
+++ Python/oefening1.py
@@ -1,1 +1,16 @@
 # elke oefening in een appart bestand
+
+#integer
+nummer = 1
+#string
+naam = "Kyrill"
+#bool
+isWaar = True
+#tupple
+tupple1 = (1, 2, 3)
+#list
+list1 = ["melk", "boter", "appel"]
+#set
+set1 = {1,2,3}
+#dict
+woordenboek = {"naam": "Kyrill", "leeftijd": 17}
